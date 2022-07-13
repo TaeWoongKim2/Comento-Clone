a9451ee60858e2c74059345974617e3515a39893
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-14 00:19:50 +0900

    fix: Context Path가 잡히지 않는 현상 수정.
    - BrowserRouter basename 속성 추가, `basename="/Comento-Clone"`.

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Comento-Clone">
     <React.StrictMode>
       <App />
     </React.StrictMode>
