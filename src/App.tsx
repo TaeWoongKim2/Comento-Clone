5ee6d49c1c08afdf95f73f79aebdd142ca405466
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-15 00:32:56 +0900

    feat: 404 Page Not Found 페이지 생성
    - 잘못된 주소로 진입할 경우, 404 Page Not Found	페이지 출력.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from './commons/layouts/MainLayout';
 import CommonLayout from './commons/layouts/CommonLayout';
+
 import Home from './pages/Home/Home';
 import CampDetails from './pages/CampDetail/CampDetails';
+
+import NotFound from './commons/errors/NotFound';
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
       <Route element={<CommonLayout />}>
         <Route path="/detail" element={<CampDetails />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
