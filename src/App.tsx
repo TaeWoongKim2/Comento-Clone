471906e95ec9d64705963ce58ac1f5ee12069a26
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-15 00:29:00 +0900

    feat: 메인 페이지 레이아웃과 이외 공통 레이아웃 생성.
    - MainLayout
    - CommonLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+import MainLayout from './commons/layouts/MainLayout';
+import CommonLayout from './commons/layouts/CommonLayout';
 import Home from './pages/Home/Home';
 import CampDetails from './pages/CampDetail/CampDetails';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details" element={<CampDetails />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Route element={<CommonLayout />}>
+        <Route path="/detail" element={<CampDetails />} />
+      </Route>
     </Routes>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,72 +2,19 @@ import React from 'react';
 
 function Home() {
   return (
-    <>
-      <header>
-        <div className="container">
-          <div className="header__navigation">
-            <span className="header__logo">Co-Lab</span>
-            <nav className="header__navigation">
-              <ul>
-                <li>🧑‍🦲</li>
-              </ul>
-            </nav>
-          </div>
-          <div className="header__combobox">
-            <h1>
-              개발은
-              <br />
-              코랩과 함께
-            </h1>
-            <div>
-              <img
-                src="https://1080motion.com/wp-content/uploads/2018/06/NoImageFound.jpg.png"
-                alt="html"
-                style={{
-                  height: '400px',
-                  width: '597.51px',
-                }}
-              />
-            </div>
-          </div>
-        </div>
-      </header>
+    <div className="container">
+      <section>
+        <h2>인기 부트 캠프</h2>
+      </section>
 
-      <div className="container">
-        <section>
-          <h2>인기 부트 캠프</h2>
-        </section>
+      <section className="text__hr">
+        <h2>현직자와 소통하며 배우는 실무 스킬, 퍼스널 트레이닝</h2>
+      </section>
 
-        <section className="text__hr">
-          <h2>현직자와 소통하며 배우는 실무 스킬, 퍼스널 트레이닝</h2>
-        </section>
-
-        <section>
-          <h2>커뮤니티</h2>
-        </section>
-      </div>
-
-      <footer>
-        <div className="container">
-          <div>
-            <h2>Co-Lab</h2>
-          </div>
-          <div>
-            대표: 내이름. 서울특별시 중구 한강대로 416, 서울스퀘어 15층 101호
-
-            Copyright by (주)카페인. All right reserved.
-            이용약관  개인정보처리방침
-          </div>
-          <div>
-            <ul>
-              <li>icon1</li>
-              <li>icon2</li>
-              <li>icon3</li>
-            </ul>
-          </div>
-        </div>
-      </footer>
-    </>
+      <section>
+        <h2>커뮤니티</h2>
+      </section>
+    </div>
   );
 }
 
