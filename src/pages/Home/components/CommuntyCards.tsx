87e2775c67cdcf4fe44a1a3719075bac0d5a0348
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-15 23:28:10 +0900

    feat: 캠프 항목 클릭 시 캠프 상세정보 페이지로 이동
    - `Link` 추가.
    - CampCard 클릭시, `/detail` 페이지로 이동.

--- src/pages/Home/components/CommuntyCards.tsx
+++ src/pages/Home/components/CommuntyCards.tsx
@@ -1,53 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function CommuntyCards() {
   return (
     <>
-      <div>
+      <Link to="/community">
         <div>
-          <span>조회수 TOP</span>
-          <span>취업 고민</span>
+          <div>
+            <span>조회수 TOP</span>
+            <span>취업 고민</span>
+          </div>
+          <p className="card__title">Title1</p>
+          <p className="card__question">Question1</p>
+          <p className="card__date">Start Date1</p>
+          <div>
+            <img
+              className="card__mento"
+              src=""
+              alt=""
+            />
+            <p className="card__mento">Mento1</p>
+            <p className="card__answer">Answer1</p>
+          </div>
         </div>
-        <p className="card__title">Title1</p>
-        <p className="card__question">Question1</p>
-        <p className="card__date">Start Date1</p>
+      </Link>
+      <Link to="/community">
         <div>
-          <img
-            className="card__mento"
-            src=""
-            alt=""
-          />
-          <p className="card__mento">Mento1</p>
-          <p className="card__answer">Answer1</p>
+          <p className="card__question">Question2</p>
+          <p className="card__title">Title2</p>
+          <p className="card__date">Start Date2</p>
+          <div>
+            <img
+              className="card__mento"
+              src=""
+              alt=""
+            />
+            <p className="card__mento">Mento2</p>
+            <p className="card__answer">Answer2</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <p className="card__question">Question2</p>
-        <p className="card__title">Title2</p>
-        <p className="card__date">Start Date2</p>
         <div>
-          <img
-            className="card__mento"
-            src=""
-            alt=""
-          />
-          <p className="card__mento">Mento2</p>
-          <p className="card__answer">Answer2</p>
+          <p className="card__title">Title3</p>
+          <p className="card__question">Question3</p>
+          <div>
+            <img
+              className="card__mento"
+              src=""
+              alt=""
+            />
+            <p className="card__mento">Mento3</p>
+            <p className="card__answer">Answer3</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <p className="card__title">Title3</p>
-        <p className="card__question">Question3</p>
-        <div>
-          <img
-            className="card__mento"
-            src=""
-            alt=""
-          />
-          <p className="card__mento">Mento3</p>
-          <p className="card__answer">Answer3</p>
-        </div>
-      </div>
+      </Link>
     </>
   );
 }
