b7953d98cfae355f150eb28803805a50a8ec532e
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-27 00:43:56 +0900

    rename: commons 폴더 구조 변경
    - Layout 및 Error 모두 페이지 폴더로 편입.

---
+++ src/pages/Layout/components/Navigation.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { ReactComponent as Profile } from 'assets/icons/profile_bk.svg';
+
+const Container = styled.header`
+  position: fixed;
+  top: 0;
+  width: -webkit-fill-available;
+  color: var(--color-white);
+  background: var(--bg-color-primary3);
+
+  nav {
+    margin: 0 auto;
+    padding: 16px;
+    max-width: 960px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .nav__logo {
+      font-size: 24px;
+      line-height: 32px;
+      letter-spacing: -0.07em;
+    }
+
+    .nav__menu {
+      font-size: 18px;
+      line-height: 32px;    
+    }
+
+    .nav__menu li {
+      display: flex;
+    }
+  }
+`;
+
+function Navigation() {
+  return (
+    <Container>
+      <nav>
+        <Link to="/" className="nav__logo">
+          Co-Lab
+        </Link>
+        <ul className="nav__menu">
+          <li>
+            <Profile />
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  );
+}
+
+export default Navigation;
