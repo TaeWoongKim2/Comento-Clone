04264c6e63769ac8beac3ff743bcd29474ac1bc8
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-27 22:55:56 +0900

    feat: Global CSS 적용.
    - styled-reset 적용
    - createGlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import GlobalStyled from 'styles/global';
 
 import Layout from './pages/Layout';
 
@@ -12,15 +13,18 @@ import NotFound from './pages/Error';
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<CampDetails />} />
-        <Route path="/apply" element={<CampApply />} />
-        <Route path="/community/:id" element={<Community />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <GlobalStyled />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<CampDetails />} />
+          <Route path="/apply" element={<CampApply />} />
+          <Route path="/community/:id" element={<Community />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
