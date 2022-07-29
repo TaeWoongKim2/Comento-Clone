b5fed2bb89deb57f8db1d1a9ea097fd763e880a2
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-29 21:56:49 +0900

    refector: className 삭제 및 스타일 컴포넌트로 변경.
    - 정석적인 방식으로 스타일 컴포넌트로 전부 변경 처리.

--- src/pages/Home/components/PopularBootCamp.tsx
+++ src/pages/Home/components/PopularBootCamp.tsx
@@ -1,42 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import Typography from 'styles/typography';
 
 import CampsFixture from 'apis/__fixture__/CampsFixture';
 import CampCards from './CampCards';
 
-const Container = styled.section`
-  margin: 0 auto 48px;
-  max-width: 960px;
-
-  .section--title {
-    font-family: 'Noto Sans';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 24px;
-    line-height: 34px;
-    letter-spacing: -0.01em;
-    color: #000000;
-  }
-
-  .section--content {
-    display: flex;
-    flex-flow: row nowrap;
-    gap: 20px;
-  }
-`;
-
 function PopularBootCamp() {
   return (
     <Container>
-      <div className="section--title">인기 부트 캠프</div>
-      <div className="section--content">
+      <SectionTitle className="section--title">
+        인기 부트 캠프
+      </SectionTitle>
+      <SectionBody>
         <CampCards
           camps={CampsFixture}
           isPopular
         />
-      </div>
+      </SectionBody>
     </Container>
   );
 }
 
 export default PopularBootCamp;
+
+const Container = styled.section`
+  margin: 0 auto 48px;
+  max-width: 960px;
+`;
+
+const SectionTitle = styled.div`
+    ${Typography.H2}
+`;
+
+const SectionBody = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 20px;
+`;
