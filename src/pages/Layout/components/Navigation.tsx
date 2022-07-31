9b0009a88817f6ca8f888799d29831805aba3c74
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-07-31 17:03:51 +0900

    design: Navigation 우측 메뉴 아이콘 스타일 적용.
    - SVG로 변경.
    - 스크롤에 따라 아이콘이 변경되도록 개선.

--- src/pages/Layout/components/Navigation.tsx
+++ src/pages/Layout/components/Navigation.tsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import colors from 'styles/colors';
 
-import { ReactComponent as Profile } from 'assets/icons/profile_bk.svg';
-
 function Navigation() {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
 
@@ -53,7 +51,19 @@ function Navigation() {
         </NavLogo>
         <NavMenu>
           <NavMenuItem>
-            <Profile />
+            {isScrolled
+              ? (
+                <svg width="17" height="20" viewBox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10.9949 7.82843C11.7451 7.07828 12.1665 6.06087 12.1665 5C12.1665 3.93913 11.7451 2.92172 10.9949 2.17157C10.2448 1.42143 9.22737 1 8.1665 1C7.10564 1 6.08822 1.42143 5.33808 2.17157C4.58793 2.92172 4.1665 3.93913 4.1665 5C4.1665 6.06087 4.58793 7.07828 5.33808 7.82843C6.08822 8.57857 7.10564 9 8.1665 9C9.22737 9 10.2448 8.57857 10.9949 7.82843Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M3.21676 14.0503C4.52951 12.7375 6.30999 12 8.1665 12C10.023 12 11.8035 12.7375 13.1163 14.0503C14.429 15.363 15.1665 17.1435 15.1665 19H1.1665C1.1665 17.1435 1.904 15.363 3.21676 14.0503Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              )
+              : (
+                <svg width="17" height="20" viewBox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M11.0671 7.82843C11.8142 7.07828 12.2338 6.06087 12.2338 5C12.2338 3.93913 11.8142 2.92172 11.0671 2.17157C10.3201 1.42143 9.30691 1 8.25045 1C7.19399 1 6.18081 1.42143 5.43378 2.17157C4.68675 2.92172 4.26707 3.93913 4.26707 5C4.26707 6.06087 4.68675 7.07828 5.43378 7.82843C6.18081 8.57857 7.19399 9 8.25045 9C9.30691 9 10.3201 8.57857 11.0671 7.82843Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M3.32127 14.0503C4.62857 12.7375 6.40165 12 8.25045 12C10.0993 12 11.8723 12.7375 13.1796 14.0503C14.4869 15.363 15.2214 17.1435 15.2214 19H1.27954C1.27954 17.1435 2.01397 15.363 3.32127 14.0503Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              )}
           </NavMenuItem>
         </NavMenu>
       </Nav>
