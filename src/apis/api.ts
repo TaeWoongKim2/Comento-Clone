35e739ecf3b8452f73d96d44b984fc7fd16cb8ec
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-08-06 01:25:07 +0900

    refector: 캠프 목록 API 메서드 리턴 값 수정.

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -4,8 +4,8 @@ import { ICampType, ICamp } from 'types/Camp';
 const API_SERVER_URL = 'https://caffein-server.herokuapp.com/api';
 
 export const fetchCamps = async (): Promise<AxiosResponse<any>> => {
-  const response = await axios.get(`${API_SERVER_URL}/camps`);
-  return response.data;
+  const { data } = await axios.get(`${API_SERVER_URL}/camps`);
+  return data;
 };
 
 export const fetchCampsByType = async (type: ICampType): Promise<AxiosResponse<ICamp[]>> => {
